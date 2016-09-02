5fb5da5c2e6fa87764b7fc3a408e00a2b60f81e0
Prabhu M Chandramouleeswaran <cprabhumouli@gmail.com>
2016-09-02 15:07:30 -0700

    Project Shell

---
+++ Src/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bikely.Startup))]
+namespace Bikely
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
